49069c067c456d42ad6d24444b8b863127dea853
RichardT74 <richtrist@gmail.com>
2019-09-02 20:34:57 +0100

    homework

---
+++ src/main/java/calculator.java
@@ -0,0 +1,22 @@
+public class calculator {
+
+
+String name;
+
+  public double add(int x, int y) {
+     return x + y;
+    }
+
+    public double subtract(int x, int y) {
+        return x - y;
+    }
+
+    public double multiply(int x, int y) {
+        return x * y;
+    }
+
+    public double divide(int x, int y) {
+        return x / y;
+    }
+
+}
---
+++ src/main/java/runner.java
@@ -0,0 +1,17 @@
+public class runner {
+
+
+    public static void main(String[] args){
+    calculator c1 = new calculator();
+
+    c1.name = "";
+    System.out.println(c1.name + c1.divide(7, 3));
+
+
+
+
+
+    }
+
+
+}
